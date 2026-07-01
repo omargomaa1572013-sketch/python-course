47086fa37cfea68d73fd4630890c318932f77972
omargomaa1572013-sketch <omargomaa1572013@gmail.com>
2026-07-01 09:25:02 -0700

    Add files via upload

---
+++ homework_4.py
@@ -0,0 +1,17 @@
+x = int(input("enter a house number: "))
+if x % 5 == 0:
+    s = x// 5
+    print(s)  
+elif x % 4 == 0:
+    s = x// 4
+    print(s)  
+elif x % 3 == 0:
+    s = x// 3
+    print(s)
+elif x % 2 == 0:
+    s = x// 2
+    print(s)
+elif x % 1 == 0:
+    s = x// 1
+    print(s)
+ 
