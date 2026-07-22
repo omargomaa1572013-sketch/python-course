b1dfa1a4e6bf2c058acdebbec649c4f55586a92f
omargomaa1572013-sketch <omargomaa1572013@gmail.com>
2026-07-22 10:34:56 -0700

    Add functionality to calculate average drink volume
    Calculate the average volume fraction of drinks based on user input.

---
+++ drinks.py
@@ -0,0 +1,9 @@
+n = int(input("enter a number of drinks : "))
+d = []
+f = 0
+for i in range(n):
+    m = int(input("enter volume fraction : "))
+    d.append(m)
+for r in m:
+    f += r
+print(f / n)
