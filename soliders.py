9a392f9120004360ab368ec0c72a700c32c6562a
omargomaa1572013-sketch <omargomaa1572013@gmail.com>
2026-07-22 07:27:36 -0700

    Add files via upload

--- soliders.py
+++ soliders.py
@@ -1,7 +1,12 @@
 x = int (input("enter a number of soliders : "))
 r = []
-for e in range(x):
-    g = int(input("enter a tall of solider : "))
-    r.append(g)
-r.sort(reverse= True)
-print(r)
+for y in range(x):
+    f = int(input("Enter soldier tall : "))
+    r.append(f)
+taller = max(r)
+shorter = min(r)
+taller == r[0]
+shorter == r[-1]
+
+steps = taller + (x - 1 - shorter)
+print(steps)
