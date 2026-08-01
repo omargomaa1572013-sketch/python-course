108d9ab62ea07153183a6772405c5ec467626a65
omargomaa1572013-sketch <omargomaa1572013@gmail.com>
2026-08-01 04:35:57 -0700

    Add files via upload

--- anton.py
+++ anton.py
@@ -1,4 +1,17 @@
-x = set(input("enter your letters : "))
+x = int(input("Enter a number: "))
 
-v = x.intersection()
-print(len(x))
+h = []
+counter = 1
+
+for i in range(x):
+    g = int(input("Enter a number: "))
+    h.append(g)
+
+for f in range(1 , x):
+    if h[f] < h[f - 1]:
+        break
+    else:
+        counter += 1
+
+print(counter)
+        
